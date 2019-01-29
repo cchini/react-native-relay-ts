43278187ce3f00157a1efc7995d40e2e69e42ba8
Giannina <giannecchini.a@gmail.com>
2019-01-29 10:16:14 -0300

    delete folders build

--- build/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import AppNavigator from './app/navigation/AppNavigator';
-const App = () => React.createElement(AppNavigator, null);
-export default App;
-//# sourceMappingURL=App.js.map
